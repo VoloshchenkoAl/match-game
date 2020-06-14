7a2b35cc2ceba092f2e6ea8d4bd06c8775c6c90f
Alex Voloshchenko <voloshchenkoal@gmail.com>
2020-06-15 00:28:42 +0300

    Updates

---
+++ precache-manifest.85132946d6e3b2e7260df7aed6f1f2f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95517effc949fea413bd61a3f45a0b6",
+    "url": "/match-game/index.html"
+  },
+  {
+    "revision": "43bf450a51b06e3ac1a3",
+    "url": "/match-game/static/css/main.684114a0.chunk.css"
+  },
+  {
+    "revision": "a066256d58e3460972cd",
+    "url": "/match-game/static/js/2.3089a854.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/match-game/static/js/2.3089a854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43bf450a51b06e3ac1a3",
+    "url": "/match-game/static/js/main.03240cf4.chunk.js"
+  },
+  {
+    "revision": "74cd26cc2cb78bcfb9a2",
+    "url": "/match-game/static/js/runtime-main.36b5221b.js"
+  }
+]);
